224b4ad0f7f4dfb4079c577262a6f060ec0fc2a2
Junaid Usmani <junaid22usmani@gmail.com>
2025-12-24 20:53:18 +0530

    adding headers in nominatim

--- utils/geoData.js
+++ utils/geoData.js
@@ -1,7 +1,11 @@
 async function getGeoData(location, country) {
     let query = `${location},${country}`;
     let nominatimUrl = `https://nominatim.openstreetmap.org/search?q=${query}&format=json&limit=1`;
-    let response = await fetch(nominatimUrl);
+    let response = await fetch(nominatimUrl, {
+        headers: {
+            'User-Agent': 'WanderLust-App-v1.0' 
+        }
+    });
     let data = await response.json();
 
     if (data.length === 0) return null;
