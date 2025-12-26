a652952927785c1a0286bcab19fbba758c2e633d
Junaid Usmani <junaid22usmani@gmail.com>
2025-12-26 10:31:16 +0530

    cleaning listing model

--- models/listing.js
+++ models/listing.js
@@ -2,10 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review.js');
 
-'https://plus.unsplash.com/premium_photo-1756131938894-83600503ca8b?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=687';
-
-const defaultUrl = 'https://png.pngtree.com/png-vector/20221125/ourmid/pngtree-no-image-available-icon-flatvector-illustration-picture-coming-creative-vector-png-image_40968940.jpg';
-
 const listingSchema = new Schema({
     title: {
         type: String,
@@ -15,7 +11,6 @@ const listingSchema = new Schema({
     image: {
         url: {
             type: String,
-            default: defaultUrl,
         },
         filename: String,
     },
